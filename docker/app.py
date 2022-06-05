854e9e823480c637fbbe8502993621a4247deb85
Hirchi <48589267+Hirchi@users.noreply.github.com>
2022-06-05 12:48:28 +0200

    Add files via upload

--- docker/app.py
+++ docker/app.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime as dt
 import os
-api_key = "2ceb1995c8cc45dbdfd4e287128c7057"
+api_key = os.environ['API_KEY']
 lat = os.environ['LAT']
 lon = os.environ['LONG']
 url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
